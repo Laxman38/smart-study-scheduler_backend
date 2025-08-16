016730e7ad15388a7fcaf66d4ced7cad876f762e
*Laxman38* <*Laxmansinghgehloth9206*>
2025-08-16 15:56:16 +0530

    Fixed start script for render development

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use('/api/progress', require('./routes/progressRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/settings', require('./routes/settingsRoutes'));
 
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(process.env.MONGO_URI, {})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => {
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
         console.log(`Server running on port ${process.env.PORT}`);
     });
 })
